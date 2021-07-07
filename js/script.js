43816c09540fb832a70aa126804fec5695d5c8e2
saadghonim <86053620+saadghonim@users.noreply.github.com>
2021-07-07 15:47:37 +0200

    edit style

--- js/script.js
+++ js/script.js
@@ -22,23 +22,50 @@ $(document).ready(function(){
 
 $('.new .owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
     nav:true,
     navText:["<span class='lnr lnr-chevron-left'></span>" ,"<span class='lnr lnr-chevron-right'></span>"],
     responsive:{
         0:{
-            items:1
+            margin:15,
+            items:2
         },
         600:{
+            margin:30,
             items:2
         },
         1000:{
+            margin:30,
             items:3
         }
 
     }
-})
+});
 
+
+if ($(window).width() <= 767) {
+
+    $(".addd_").addClass("owl-carousel");
+    $(".addd_").removeClass("row");
+    $('.addd_.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:false,
+        touchDrag  : false,
+        mouseDrag  : false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+        }
+    })
+
+}
 
 
 });
